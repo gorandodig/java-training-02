1ab3c2a8468eac511b422f1cbecb7f54ecd95ca6
a-marktl <a.marktl@ssi-schaefer-peem.com>
2013-06-18 07:33:58 +0200

    - removed obsolete file - updated ip address used (=loopback)

--- nio-webserver/src/at/amarktl/Main.java
+++ nio-webserver/src/at/amarktl/Main.java
@@ -12,19 +12,19 @@ public class Main {
     int portRMI = 1099;
     String serverAddress = "127.0.0.1";
 
-    Server s = new Server("10.110.20.212", portRMI, 8080, 10);
+    Server s = new Server("127.0.0.1", portRMI, 8080, 10);
     s.start();
 
-    IClusterNode n1 = new Node("N1", "10.110.20.212", 2099, 5);
+    IClusterNode n1 = new Node("N1", "127.0.0.1", 2099, 10);
     n1.connect(serverAddress, portRMI);
 
-    IClusterNode n2 = new Node("N2", "10.110.20.212", 2199, 5);
+    IClusterNode n2 = new Node("N2", "127.0.0.1", 2199, 10);
     n2.connect(serverAddress, portRMI);
 
-    IClusterNode n3 = new Node("N3", "10.110.20.212", 2299, 5);
+    IClusterNode n3 = new Node("N3", "127.0.0.1", 2299, 10);
     n3.connect(serverAddress, portRMI);
 
-    IClusterNode n4 = new Node("N4", "10.110.20.212", 2399, 5);
+    IClusterNode n4 = new Node("N4", "127.0.0.1", 2399, 5);
     n4.connect(serverAddress, portRMI);
 
   }
